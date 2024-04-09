73af5963f314b83cba042abf2a77cc5c57b55fb0
saurabhkav03 <kaverisaurabh1998@gmail.com>
2024-04-10 00:42:51 +0530

    Created new files

---
+++ scripts/start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+echo "Pulling docker Image"
+docker pull kaveri03/simple-python-flask-app:latest
+
+# Run the Docker image as a container
+echo "Running the docker images as a container"
+docker run --d --publish 5000:5000 kaveri03/simple-python-flask-app:latest
