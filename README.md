46313f8617c0248aaeac73b329a2192bb5084e37
saurabhkav03 <kaverisaurabh1998@gmail.com>
2024-04-09 17:38:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple_python_app
+ We'll create an AWS CodePipeline to automate the CICD process for our Python application. 
---
+++ app.py
@@ -0,0 +1,15 @@
+# This code is a simple Python script that uses the Flask framework to create a basic web application.
+# This line imports the Flask class from the flask module. Flask is a lightweight web application framework for Python.
+from flask import Flask
+
+# Here, we create an instance of the Flask class and pass __name__ as an argument. The __name__ variable is a special Python variable that represents the name of the current module. This is used by Flask to determine the root path of the application.
+app = Flask(__name__)
+
+# This code defines a route / using the @app.route() decorator. When a request is made to the root URL of the application (/), Flask will invoke the hello() function. This function returns the string 'Hello, world!', which will be sent as the response to the client.
+@app.route('/')
+def hello():
+    return 'Hello, world!'
+
+# This conditional statement ensures that the Flask web server (app.run()) is only started if the script is executed directly (not imported as a module). When you run this script from the command line, Flask starts a development server on your local machine, listening for incoming HTTP requests on the default port (usually 5000).
+if __name__ == '__main__':
+    app.run()
